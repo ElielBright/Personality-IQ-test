9e3c7570e86443dac41167c9c9411625c806a146
Eliel Kwesi Agbeke <brighteliel@gmail.com>
2026-06-09 23:21:11 +0000

    Move Firebase config to environment variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore
 import { getAuth, signInAnonymously } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeOhX_qvMsf1SnGjSBVN29jpWQVbhX9uQ",
-  authDomain: "edify-bible-app.firebaseapp.com",
-  projectId: "edify-bible-app",
-  storageBucket: "edify-bible-app.firebasestorage.app",
-  messagingSenderId: "39135039352",
-  appId: "1:39135039352:web:2d822b42a8dad7d11e028d"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
